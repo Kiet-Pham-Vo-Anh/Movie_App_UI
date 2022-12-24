2f0de803f4a9c28d54b15a324f3466870d1b9739
KietPham <kietpva0102@gmail.com>
2022-12-24 13:39:14 +0700

    HomeScreen: Categories

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_ui/constants.dart';
+import 'package:movie_app_ui/screens/home/components/categories.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        CategoryList(),
+      ],
+    );
+  }
+}
